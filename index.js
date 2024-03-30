a59830369a40738142d9b1734d23063751a8f975
Fayaz ALI <92179108+1fayaz@users.noreply.github.com>
2024-03-30 23:59:18 +0500

    frist I wwrite node_modules in Gitignore file but forgot to save it where the terminal make a very large command while in git add .

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter frist number", type: "number", name: "fristNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.fristNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.fristNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.fristNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.fristNumber / answer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
+;
